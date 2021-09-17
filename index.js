d7f0cdf81fb539629e3e1dde8475d4f73d5fc314
sagarmounika <sagarmouni5@gmail.com>
2021-09-18 00:54:07 +0530

    added solutions

---
+++ index.js
@@ -0,0 +1,65 @@
+function countWords(str, word1, word2) {
+  let a = str.split(" ")
+  let count1 = 0
+  let count2 = 0
+  for (let i = 0; i < a.length; i++) {
+    if (word1 === a[i]) {
+      count1++
+    }
+
+    if (word2 === a[i]) {
+      count2++
+    }
+  }
+
+  return count1 > count2
+    ? `The word ${word1} more frequently occurred than ${word2}.`
+    : `The word ${word2} more frequently occurred than ${word1}.`
+}
+
+const paragraph =
+  "I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love."
+
+function countWordsFiltered(sentence) {
+  let a = sentence.split(" ").filter(x => x.length !== 1)
+
+  let count = 0
+
+  for (let i = 0; i < a.length; i++) {
+    count++
+  }
+  return count
+}
+const sentence = `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
+
+
+function sevenRandomNumbers() {
+  let arr = []
+  while (arr.length < 8) {
+    var r = Math.floor(Math.random() * 9)
+    if (arr.indexOf(r) === -1) arr.push(r)
+  }
+  return arr
+}
+
+
+function reverseArray(input) {
+  var ret = new Array()
+  for (var i = input.length - 1; i >= 0; i--) {
+    ret.push(input[i])
+  }
+  return ret
+}
+
+document.getElementById("app").innerHTML = `
+
+<div>
+
+<div> ${countWords(paragraph, "love", "you")}</div> 
+<div> ${countWordsFiltered(sentence)}</div> 
+<div>${sevenRandomNumbers()}</div> 
+<div>${reverseArray(["A", "B", "C"])}</div> 
+
+
+</div>
+`
